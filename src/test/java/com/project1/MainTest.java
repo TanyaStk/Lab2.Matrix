296cdd1595c4a8b37448cc707fcdd237311b9c71
TanyaStk <64659217+TanyaStk@users.noreply.github.com>
2020-09-18 00:19:52 +0300

    MainDevelop

--- src/test/java/com/project1/MainTest.java
+++ src/test/java/com/project1/MainTest.java
@@ -1,4 +1,23 @@
 package com.project1;
 
+import org.junit.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+    @org.junit.jupiter.api.Test
+    void testSortMatrixByNonDecreasingSums() {
+    }
+
+    @org.junit.jupiter.api.Test
+    private void testCountSumOfElementsInLine() {
+        int[] array = new int[] {1, 2, 3, 4, 5};
+        int expected = 15;
+        //int actual = countSumOfElementsInLine(array);
+        //assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSwapMatrixLines() {
+    }
 }
